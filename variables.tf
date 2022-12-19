942bd9d991f9f59d0e314a2601c3ea2ff5e8e929
Vinícius G. Oliveira <vinicius.oliver@gmail.com>
2022-12-19 15:39:05 -0300

    feat(*): Adjust vars

--- variables.tf
+++ variables.tf
@@ -1,17 +1,14 @@
 variable "region" {
   type        = string
   description = "The region in which to create/manage resources"
-  default     = "us-east-1"
 }
 
 variable "profile" {
   type        = string
   description = "The AWS profile to use"
-  default     = "AWSPersonalVINI"
 }
 
 variable "account_id" {
   type        = string
   description = "The account ID in which to create/manage resources"
-  default     = "934954887435"
 }
